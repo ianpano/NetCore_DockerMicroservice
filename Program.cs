4eb338fb07ec53efb0c1b42ef58712fb3b3e2d6b
ianpano@gmail.com <ianpano@gmail.com>
2019-01-12 08:26:30 +0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using HelloWorld;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace NetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+            .AddCommandLine(args)
+            .Build();
+
+            var host = new WebHostBuilder()
+            .UseKestrel()
+            .UseStartup<Startup>()
+            .UseConfiguration(config)
+            .Build();
+
+            host.Run();
+        }
+    }
+}
